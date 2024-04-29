db7537830169e3930a29754180b996c571ac6ec1
Coach Chuck <christian.krueger.health@gmail.com>
2024-04-29 12:49:21 -0600

    added starting code

--- tests/set-authority-example.ts
+++ tests/set-authority-example.ts
@@ -1,17 +1,27 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaLabCpiGuard } from "../target/types/solana_lab_cpi_guard";
-import { PublicKey } from '@solana/web3.js'
+import { LAMPORTS_PER_SOL } from '@solana/web3.js'
 import { disableCpiGuard, createMint, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token'
 // import { createTokenAccountWithCPIGuard } from "./token-helper"; // We'll import this later
 import { assert } from "chai"
+import { airdropIfRequired, makeKeypairs } from "@solana-developers/helpers";
 
 describe("approve-delegate-test", () => {
-    // Configure the client to use the local cluster.
-    anchor.setProvider(anchor.AnchorProvider.env());
 
     const program = anchor.workspace.SolanaLabCpiGuard as Program<SolanaLabCpiGuard>;
     const provider = anchor.AnchorProvider.env();
+    const payer = (provider.wallet as anchor.Wallet).payer;
+    const connection = provider.connection;
+
+    anchor.setProvider(provider);
+
+    // test accounts
+    const [testTokenMint, userTokenAccount, maliciousAccount] = makeKeypairs(3);
+
+    before(async () => {
+        await airdropIfRequired(connection, payer.publicKey, LAMPORTS_PER_SOL * 2, LAMPORTS_PER_SOL);
+    })
 
     it("[CPI Guard] Set Authority Example", async () => {})
 
