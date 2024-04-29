91d639058a4a2ac90c7f260d53cbba3aa9a24d28
Coach Chuck <christian.krueger.health@gmail.com>
2024-04-29 13:33:14 -0600

    signed off

--- tests/set-owner-example.ts
+++ tests/set-owner-example.ts
@@ -7,6 +7,7 @@ import {
   AuthorityType,
   createMint,
   TOKEN_2022_PROGRAM_ID,
+  createAccount,
 } from "@solana/spl-token";
 // import { createTokenAccountWithCPIGuard } from "./token-helper"; // We'll import this later
 import { assert } from "chai";
@@ -23,7 +24,13 @@ describe("set-owner-test", () => {
   anchor.setProvider(provider);
 
   // test accounts
-  const [testTokenMint, userTokenAccount, newOwner] = makeKeypairs(3);
+  const [
+    testTokenMint,
+    userTokenAccount,
+    firstNonCPIGuardAccount,
+    secondNonCPIGuardAccount,
+    newOwner,
+  ] = makeKeypairs(5);
 
   before(async () => {
     await airdropIfRequired(
@@ -54,28 +61,94 @@ describe("set-owner-test", () => {
     );
 
     try {
-        await setAuthority(
-            provider.connection,
-            payer,
-            userTokenAccount.publicKey,
-            payer,
-            AuthorityType.AccountOwner,
-            newOwner.publicKey,
-            undefined,
-            undefined,
-            TOKEN_2022_PROGRAM_ID
-        )
-    } catch(e) {
-        assert(e.message == "failed to send transaction: Transaction simulation failed: Error processing Instruction 0: custom program error: 0x2f")
-        console.log("Account ownership cannot be changed while CPI guard is enabled.")
+      await setAuthority(
+        provider.connection,
+        payer,
+        userTokenAccount.publicKey,
+        payer,
+        AuthorityType.AccountOwner,
+        newOwner.publicKey,
+        undefined,
+        undefined,
+        TOKEN_2022_PROGRAM_ID
+      );
+    } catch (e) {
+      assert(
+        e.message ==
+          "failed to send transaction: Transaction simulation failed: Error processing Instruction 0: custom program error: 0x2f"
+      );
+      console.log(
+        "Account ownership cannot be changed while CPI guard is enabled."
+      );
     }
   });
 
   it("Set Authority without CPI on Non-CPI Guarded Account", async () => {
-    
+    await createAccount(
+      provider.connection,
+      payer,
+      testTokenMint.publicKey,
+      payer.publicKey,
+      firstNonCPIGuardAccount,
+      undefined,
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    await setAuthority(
+      provider.connection,
+      payer,
+      firstNonCPIGuardAccount.publicKey,
+      payer,
+      AuthorityType.AccountOwner,
+      newOwner.publicKey,
+      undefined,
+      undefined,
+      TOKEN_2022_PROGRAM_ID
+    );
+  });
+  it("[CPI guard] Set Authority via CPI on CPI Guarded Account", async () => {
+    try {
+      await program.methods
+        .setOwner()
+        .accounts({
+          authority: payer.publicKey,
+          tokenAccount: userTokenAccount.publicKey,
+          newOwner: newOwner.publicKey,
+          tokenProgram: TOKEN_2022_PROGRAM_ID,
+        })
+        .signers([payer])
+        .rpc();
+    } catch (e) {
+      assert(
+        e.message ==
+          "failed to send transaction: Transaction simulation failed: Error processing Instruction 0: custom program error: 0x2e"
+      );
+      console.log(
+        "CPI guard is enabled, and a program attempted to add or change an authority."
+      );
+    }
   });
 
-  it("[CPI Guard] Set Authority via CPI on CPI Guarded Account", async () => {});
+  it("Set Authority via CPI on Non-CPI Guarded Account", async () => {
+    await createAccount(
+      provider.connection,
+      payer,
+      testTokenMint.publicKey,
+      payer.publicKey,
+      secondNonCPIGuardAccount,
+      undefined,
+      TOKEN_2022_PROGRAM_ID
+    );
 
-  it("Set Authority via CPI on Non-CPI Guarded Account", async () => {});
+    await program.methods
+      .setOwner()
+      .accounts({
+        authority: payer.publicKey,
+        tokenAccount: secondNonCPIGuardAccount.publicKey,
+        newOwner: newOwner.publicKey,
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
+      })
+      .signers([payer])
+      .rpc();
+  });
 });
